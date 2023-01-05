c9344f5d7c5fcdb9f393fa205cd239b7355cec4d
ridowayne <hassanrilwan12@gmail.com>
2023-01-05 20:47:49 +0100

    Feat: updated the docs and started the uploading feature

--- utils/upload.js
+++ utils/upload.js
@@ -1,2 +1,11 @@
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const ErrorResponse = require('./appError');
+
+const storage = multer.memoryStorage();
+const multerFilter = (req, file) => {
+  if (!file || !req.body) {
+    throw new ErrorResponse('file not found', 404);
+  }
+  if (file?.fieldname === 'receipt') {
+  }
+};
