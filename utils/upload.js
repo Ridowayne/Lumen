16181ec9ccdc52c4049ef24b13d4659113dc180e
ridowayne <hassanrilwan12@gmail.com>
2023-01-09 20:56:16 +0100

    Feat: Added uploading with multer for all uploads like receipts and all

--- utils/upload.js
+++ utils/upload.js
@@ -1,11 +1,59 @@
 const multer = require('multer');
+const sharp = require('sharp');
+const cloudinary = require('cloudinary').v2;
 const ErrorResponse = require('./appError');
 
-const storage = multer.memoryStorage();
-const multerFilter = (req, file) => {
-  if (!file || !req.body) {
-    throw new ErrorResponse('file not found', 404);
-  }
-  if (file?.fieldname === 'receipt') {
+cloudinary.config({
+  cloud_name: 'dsuwyk6ml',
+  api_key: '975589453365429',
+  api_secret: 'nFRxoqatCbMsy0RCpYfrLfzB1wI',
+});
+
+const multerStorage = multer.memoryStorage();
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(
+      new ErrorResponse('Not an image! Please upload only images.', 400),
+      false
+    );
   }
 };
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+const resizeImage = async (req, res, next) => {
+  if (!req.file) return next();
+  req.file.fileName = `receipt-${req.body.uniqueCode}-${Date.now()}.jpeg`;
+
+  await sharp(req.file.buffer)
+    .resize(400, 400)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toBuffer()
+    .then((data) => {
+      console.log(data);
+      cloudinary.uploader
+        .upload(
+          data,
+          {
+            public_id: req.file.fileName,
+            tags: 'image_upload',
+          },
+          (err, image) => {
+            if (err) {
+              return res.status(400).send(err);
+            }
+            // req.body.receiptOfPayment = image
+            return image;
+          }
+        )
+        .catch((err) => {
+          res.status(400).send(err);
+        });
+    });
+};
+module.exports = { upload, resizeImage };
